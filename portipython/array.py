c77fe8a6c16e614429135ec8ea5583c667f2d989
Daniel <167945354+Kurosakiniel@users.noreply.github.com>
2024-11-05 16:14:22 -0300

    Create array.py

---
+++ portipython/array.py
@@ -0,0 +1,20 @@
+# Crianda um array de jogos
+
+MeusJogos = ['God of war 1', 'God of war 2' , 'Tekken' ,'God of war 3' , 'God of war 2018' ,  
+   'Fifa' , 'God of war Ragnarok', 'God of war Ghost of Sparta']
+
+# Adicionando jogo no Array usando Append
+
+MeusJogos.append('God of war 3 Ascension')
+
+# Removendo jogo usando Remove
+
+MeusJogos.remove('Tekken')
+
+# Substituir itens usando o indice e uma nova variavel
+
+JogoRuim = 'God of war Chains of Olympus'
+
+MeusJogos[4] = JogoRuim
+
+print(MeusJogos)
